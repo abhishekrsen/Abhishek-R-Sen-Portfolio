12534625c59faa8ae72c57886b372488f16cda77
abhisheekrsen <abhishekrsen.in@gmail.com>
2026-06-10 11:26:47 +0530

    Add profile picture to Hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import profilePic from '../assets/profile.png';
 import './Hero.css';
 
 const Hero = () => {
@@ -17,6 +18,9 @@ const Hero = () => {
             <a href="#contact" className="btn btn-outline" style={{ marginLeft: '1rem' }}>Contact Me</a>
             <a href="https://drive.google.com/file/d/1-Oem3aK4jWU1Qd_Bnsx2eEi_a-Ma00Ub/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="btn btn-outline" style={{ marginLeft: '1rem' }}>View My Resume</a>
           </div>
+        </div>
+        <div className="hero-image-wrapper animate-fade-in-up delay-300">
+          <img src={profilePic} alt="Abhishek R Sen" className="hero-image" />
         </div>
       </div>
     </section>
